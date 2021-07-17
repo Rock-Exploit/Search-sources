7d3b53dd2670f52c9464d1375b78c0420d6642d7
Rock-Exploit <85040222+Rock-Exploit@users.noreply.github.com>
2021-07-17 03:24:27 +0300

    Create search.py

---
+++ search.py
@@ -0,0 +1,27 @@
+import webbrowser
+print('''/----//__|-^\\\\\\\\
+       _.---._    /\\
+    ./'       "--`\//
+  ./              o \          .-----.
+ /./\  )______   \__ \        ( help! )
+./  / /\ \   | \ \  \ \       /`-----'
+   / /  \ \  | |\ \  \7---  Twitter:msf6_Exploit
+''')
+
+
+print("1-google dorks")
+print("2-black hat python")
+print("3-oscp")
+print("4-Python Projects ")
+print("5-Telegram channel")
+bock=int(input("Enter number:"))
+if bock ==1:
+    webbrowser.open("https://pentest-tools.com/information-gathering/google-hacking") 
+elif bock ==2:
+        webbrowser.open("https://olinux.net/wp-content/uploads/2019/01/python.pdf")
+elif bock ==3:
+         webbrowser.open("https://www.youtube.com/watch?v=bc8JcenrvB0&list=PL_yseowcuqYJc7wXtGIsshYp1B_W0M-ZK")
+elif bock ==4:
+            webbrowser.open("https://www.youtube.com/watch?v=p53vqiTnzNw&list=PLuXY3ddo_8nzCVqXcTFqwcM5R0gZiMRiW")
+elif bock ==5:
+    webbrowser.open("https://t.me/rcsubsunb")
